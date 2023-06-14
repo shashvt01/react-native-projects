b1a672773063e651d3641b978cf1e8126f897dd7
shasvt01 <shashwatv07@gmail.co>
2023-06-14 16:19:56 +0530

    first react-native project

---
+++ Awesome01/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Button,
+} from 'react-native'
+
+function App(){
+  return(
+    <SafeAreaView>
+      <View>
+        <Text>Hello Wrold!</Text>
+        <Text>Hello Wrold!</Text>
+        <Text>Hello Wrold!</Text>
+        <Text>Hello Wrold!</Text>
+        <Text>Hello Wrold!</Text>
+
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default App;
